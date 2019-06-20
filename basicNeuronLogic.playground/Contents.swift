2754e150738914f00e63e10cc19ef60db4b8baa2
Marco Dalprato <marcodalprato@me.com>
2019-06-20 22:48:27 +0200

    Improved functions

--- basicNeuronLogic.playground/Contents.swift
+++ basicNeuronLogic.playground/Contents.swift
@@ -4,10 +4,7 @@
 import UIKit
 import PlaygroundSupport
 
-let inputsData = [[0,0,1],[1,1,1],[1,0,1],[0,1,1]]
-let outputsData = [0,1,1,0]
-
-let predictionInput = [1,0,0]
+//https://mattmazur.com/2015/03/17/a-step-by-step-backpropagation-example/
 
 func sigmoidFunc(input : Double) -> Double {
     return (1 / ( 1 + (pow(M_E, -input)) ))
@@ -20,62 +17,64 @@ func randomizeInput(inputData: Int) -> Double{
     return (Double(inputData * random))
 }
 
-func neuron(input1: Int, input2: Int, input3: Int) -> Float{
-    
-    let random = Int.random(in: -10...10)
-    
-    
-    let random1 = (input1 * random)
-    let random2 = (input2 * random)
-    let random3 = (input3 * random)
 
-    
-    let out = Float(random1 + random2 + random3)
-    
-    
-   // print ("\(input1) * \(random1) +  \(input2) * \(random2) +  \(input3) * \(random3)")
-    
-    return out
-    
-}
-
-
-func assignWeight(){
-    
-    for inputs in inputsData {
-    
+func calculateNeuron(w1: Double, i1: Double, w2: Double, i2: Double, b: Double, n: Double) -> Double{
  
-      let out =  neuron(input1: inputs[0], input2: inputs[1], input3: inputs[2])
-        
-    
-        let sigmoidOut = sigmoidFunc(input: Double(out))
-      
-        print (sigmoidOut)
-      
-        
-    }
+    let neth1 = w1*i1 + w2*i2 + b*n
+    let outh1 = sigmoidFunc(input: neth1)
+    return outh1
     
 }
 
-func trainLoop(){
-
-    for test in 0...3 {
-        print ("--")
-        assignWeight()
-          print ("--")
-    }
+func calculateError(target: Double, output: Double) -> Double{
+    
+    let targetLessOutut = (target - output)
+    let output = (1/2) * (targetLessOutut * targetLessOutut)
+    
+    return output
 }
 
-trainLoop()
-
-/*
-func adjustWeight() -> Double{
+func calculateTotalError(error1: Double, error2:Double) -> Double{
     
-    
-    let out = error * input * output * (1 - output)
+    return error1 + error2
     
     
 }
- */
+
+func network(weight1:Double,weight2: Double, weight3:Double, weight4: Double, weight5:Double, weight6:Double, weight7:Double, weight8:Double,
+             input1:Double, input2: Double,
+             bias1:Double, neutro1: Double,
+             bias2:Double, neutro2:Double,
+             targetOut1: Double, targetOut2:Double) -> Double{
+    
+    let outH1 =   calculateNeuron(w1: weight1, i1: input1, w2: weight2, i2: input2, b: bias1, n: neutro1)
+    let outH2 =   calculateNeuron(w1: weight3, i1: input1, w2: weight4, i2: input2, b: bias1, n: neutro1)
+    let outHO1 =  calculateNeuron(w1: weight5, i1: outH1, w2: weight6, i2: outH2, b: bias2, n: neutro2)
+    let outHO2 =  calculateNeuron(w1: weight7, i1: outH1, w2: weight8, i2: outH2, b: bias2, n: neutro2)
+    let errorO1 = calculateError(target: targetOut1, output: outHO1)
+    let errorO2 = calculateError(target: targetOut2, output: outHO2)
+    let errorTotal = calculateTotalError(error1: errorO1, error2: errorO2)
+    
+
+    print ("outH1 = \(outH1) -- outH2 = \(outH2)")
+    print ("outHO1 = \(outHO1) -- outHO2 = \(outHO2)")
+    print ("errorO1 = \(errorO1) -- errorO2 = \(errorO2) -- errorTotal \(errorTotal)")
+
+    return errorTotal
+}
+
+func main(){
+    
+    
+    let total = network(weight1: 0.15, weight2: 0.20, weight3: 0.25, weight4: 0.30,
+                        weight5: 0.40, weight6: 0.45, weight7: 0.50, weight8: 0.55,
+                        input1: 0.05, input2: 0.10, bias1: 0.35, neutro1: 1,
+                        bias2: 0.60, neutro2: 1, targetOut1: 0.01, targetOut2: 0.99)
+    
+    print(total)
+
+    
+}
 
 
+main()
